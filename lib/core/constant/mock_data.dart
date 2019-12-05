99d1a3149f1a6d9cb5680f5435dd064e0f15047e
Mukhtar Fauzi <mukhtar.fz@gmail.com>
2019-12-05 23:19:25 +0700

    create mock data because API is error

---
+++ lib/core/constant/mock_data.dart
@@ -0,0 +1,26 @@
+const ingredientsData = [
+  {
+    "title" : "Meat",
+    "use-by": "2020-11-12"
+  },
+  {
+    "title" : "Egg",
+    "use-by": "2019-11-12"
+  },
+  {
+    "title" : "Milk",
+    "use-by": "2019-01-12"
+  },
+  {
+    "title" : "Ketchup",
+    "use-by": "2019-12-12"
+  },
+  {
+    "title" : "Ham",
+    "use-by": "2013-12-12"
+  },
+  {
+    "title" : "Cheese",
+    "use-by": "2019-12-12"
+  },
+];
