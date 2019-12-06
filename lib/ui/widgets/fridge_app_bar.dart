672635aa1f45f2a5621a70559e50f26783f31870
Mukhtar Fauzi <mukhtar.fz@gmail.com>
2019-12-06 08:16:45 +0700

    add some custom parameter

--- lib/ui/widgets/fridge_app_bar.dart
+++ lib/ui/widgets/fridge_app_bar.dart
@@ -3,6 +3,15 @@ import 'package:provider/provider.dart';
 import 'package:tech_task/core/view_models/recipes_suggestion_model.dart';
 
 class FridgeAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final String subtitle;
+  final bool enableDatePicker;
+
+  const FridgeAppBar({Key key,
+    @required this.title,
+    @required this.subtitle,
+    this.enableDatePicker : true
+  }) : super(key: key);
 
   void _showLunchDatePicker(
       BuildContext context, RecipeSuggestionModel model) async {
@@ -16,65 +25,70 @@ class FridgeAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Consumer<RecipeSuggestionModel>(
-        builder: (__, model, _) => Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                  flex: 3,
-                  child: Row(
-                    children: <Widget>[
-                      Flexible(
-                        child: FittedBox(
-                          child: Text('My Lunch Suggestion',
-                              style: Theme.of(context).textTheme.display1),
+    return Material(
+      elevation: 0.8,
+      child: SafeArea(
+        child: Consumer<RecipeSuggestionModel>(
+          builder: (__, model, _) => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                    flex: 3,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        if(ModalRoute.of(context).canPop)BackButton(),
+                        Flexible(
+                          child: FittedBox(
+                            child: Text(title,
+                                style: Theme.of(context).textTheme.display1),
+                          ),
                         ),
-                      ),
-                      SizedBox(
-                        width: 16,
-                      ),
-                      InkWell(
-                        onTap: () => _showLunchDatePicker(context, model),
-                        child: Material(
-                          color: Colors.transparent,
-                          child: Padding(
-                            padding: const EdgeInsets.all(2.0),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: <Widget>[
-                                Text(
-                                  model.getLunchDate.replaceAll('-', '/'),
-                                  style: TextStyle(fontWeight: FontWeight.w700),
-                                ),
-                                SizedBox(
-                                  width: 3,
-                                ),
-                                Icon(
-                                  Icons.settings,
-                                  size: 16,
-                                ),
-                              ],
+                        SizedBox(
+                          width: 16,
+                        ),
+                        if(enableDatePicker)InkWell(
+                          onTap: () => _showLunchDatePicker(context, model),
+                          child: Material(
+                            color: Colors.transparent,
+                            child: Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: <Widget>[
+                                  Text(
+                                    model.getLunchDate.replaceAll('-', '/'),
+                                    style: TextStyle(fontWeight: FontWeight.w700),
+                                  ),
+                                  SizedBox(
+                                    width: 3,
+                                  ),
+                                  Icon(
+                                    Icons.settings,
+                                    size: 16,
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  )),
-              Spacer(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  Text(
-                    'Ingredients: ',
-                    style: Theme.of(context).textTheme.subhead,
-                  ),
-                ],
-              )
-            ],
+                      ],
+                    )),
+                Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      subtitle,
+                      style: Theme.of(context).textTheme.subhead,
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
